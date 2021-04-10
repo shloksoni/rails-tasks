ac63b2355723f723645a440017c6cc18a2c35366
Shlok <s2shlok@gmail.com>
2021-04-10 14:08:59 +0530

    Added API based architecture

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,20 +1,7 @@
-import React, { useEffect } from "react";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import React from "react";
 
 const App = () => {
-  useEffect(() => {
-    initializeLogger();
-    logger.info("Log from babel js-logger");
-  }, []);
-
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
-      </Switch>
-    </Router>
-  );
+  return <h1>This is App.jsx</h1>;
 };
 
 export default App;
