f11925ec9f9cf6a3472c86d201bbcf3fbde747dc
Shlok <s2shlok@gmail.com>
2021-04-10 15:47:14 +0530

    Implemented create action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  protect_from_forgery with: :null_session
   def index
     render
   end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,25 @@
 class TasksController < ApplicationController
+  
+  protect_from_forgery with: :null_session
+
   def index
     tasks = Task.all
     render status: :ok, json: { tasks: tasks }
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render status: :ok, json: { notice: t('successfully_created')  }
+    else
+      errors = @task.errors.full_messages
+      render status: :unprocessable_entity, json: { errors: errors  }
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title)
   end
 end
