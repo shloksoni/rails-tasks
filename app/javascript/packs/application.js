3bac7e53020b444fe83143a2c23b18bfcaeff45f
Shlok <s2shlok@gmail.com>
2021-04-13 00:58:43 +0530

    Removed empty string from custom.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start();
 require("@rails/activestorage").start();
 require("channels");
 import "../stylesheets/application.scss";
-
-// Support component names relative to this directory:
 var componentRequireContext = require.context("src", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
